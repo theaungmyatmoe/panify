eaf1490f7aac8670ffd52053bde5c84d05c732bb
amm834 <aungmyatmoe834@gmail.com>
2021-06-11 03:20:56 +0630

    Update:Code Style

---
+++ src/randomRng.js
@@ -0,0 +1,17 @@
+export const randRng = (min, max) => {
+  if (min < 0) {
+    return Math.floor(Math.random() * max);
+  } else {
+    return Math.floor(Math.random() * (max + min));
+  }
+};
+
+export const randDate = () => {
+  const now = new Date();
+  const month = Math.floor(Math.random() * 12)  + 1;
+  const year = now.getFullYear() + Math.floor(Math.random() *  5);
+  return {
+    month,
+    year
+  }
+}
