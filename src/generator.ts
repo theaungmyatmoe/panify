626657888a4b5d2320bde046bfc70f7935d559e0
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-07-16 17:58:14 +0630

    refactor: change to treeshakable

--- src/generator.ts
+++ src/generator.ts
@@ -1,42 +1,40 @@
-import {generateRandomData, generateRandomRange} from './utils/random';
-import {validate} from "@amm834/luhn";
+import { Card } from "./contracts";
+import { generateRandomData, generateRandomRange } from "./utils";
+import { validate } from "@amm834/luhn";
 
-type Card = {
-    cardNumber: number;
-    month: number;
-    year: number;
+interface Generator {
+  generate: () => Promise<Card[]>;
 }
 
-
 /*
-* @param {number} cli - The first 6 digits of the card number
-* @param {number} total - The total number of card numbers that you want to generate
-* @returns {void}
-* */
-export function createGenerator(bin: number, total = 10) {
-    const generate = async (): Promise<Card[]> => {
-        const cards: Card[] = [];
+ * @param {number} bin - The first 6 digits of the card number
+ * @param {number} limit - The total number of cards
+ * @returns {Generator} - A generator object
+ */
+export function createGenerator(bin: number, limit = 10): Generator {
+  const generate = async () => {
+    const cards: Card[] = [];
 
-        for (; ;) {
-            const generatedRandomNumber = `${bin}${generateRandomRange(0, 1000)}`
+    for (;;) {
+      const generatedRandomNumber = `${bin}${generateRandomRange(0, 1000)}`;
 
-            if (validate(generatedRandomNumber)) {
-                const {month, year} = generateRandomData();
+      if (validate(generatedRandomNumber)) {
+        const { month, year } = generateRandomData();
 
-                const validateCard: Card = {
-                    cardNumber: +generatedRandomNumber,
-                    month,
-                    year,
-                };
+        const validateCard: Card = {
+          cardNumber: +generatedRandomNumber,
+          month,
+          year,
+        };
 
-                cards.push(validateCard);
-            }
-            if (cards.length === total) break;
-        }
-        return cards;
-    };
-
-    return {
-        generate,
+        cards.push(validateCard);
+      }
+      if (cards.length === limit) break;
     }
+    return cards;
+  };
+
+  return {
+    generate,
+  };
 }
